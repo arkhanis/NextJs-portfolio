9b81ee412bfbc01b7fd378a6e893b6d8e68c2d01
Cesar Casanova <68834617+arkhanis@users.noreply.github.com>
2024-02-29 15:19:00 -0300

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import profileImg from "@/public/foto_profile.png";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -32,7 +33,7 @@ export default function Intro() {
           >
             <Image
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              alt="Cesar Casanova portrait"
               width="192"
               height="192"
               quality="95"
